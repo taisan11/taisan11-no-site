6be401e059ae2e3bd00d8e8031a9ef9ddb1c171b
taisan11 <131413638+taisan11@users.noreply.github.com>
2024-12-30 09:33:55 +0900

    u-nn

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro'
 
+import sitemap from '@astrojs/sitemap';
+
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations:[UnoCSS()],
+    integrations:[UnoCSS(), sitemap(), playformCompress()],
     prefetch:true
 });
