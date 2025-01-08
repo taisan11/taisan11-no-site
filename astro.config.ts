5b833612b4a6481be3b19d6789727c4b4bbc5803
taisan11 <131413638+taisan11@users.noreply.github.com>
2025-01-08 17:20:04 +0900

    fix

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro'
 import sitemap from '@astrojs/sitemap';
 import playformCompress from '@playform/compress';
+import aaa from "vite-budoux-build/astro"
 
 // https://astro.build/config
 export default defineConfig({
-    integrations:[UnoCSS(), sitemap(), playformCompress()],
+    integrations:[
+        UnoCSS(),
+        sitemap(),
+        // playformCompress(),
+        aaa()
+    ],
     prefetch:true
 });
